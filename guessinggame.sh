f6b52c819dd46b1a0adf75d67aaf66c22f390899
Rebeca <80820796+becabazinga73@users.noreply.github.com>
2021-06-07 00:54:33 -0300

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -3,17 +3,17 @@
 
 function guess_file_count {
 
-      correct_guess=3
+      correct_guess=0
       do
 echo "Type in a guess and then press Enter:"
 read guess
 if [[ $guess =~ ^/s*$ || $guess =~ [^0-9]+ ]]
 then
 echo "Your guess should be a number!"
-elif [[ $guess -gt 3 ]]
+elif [[ $guess -gt 0 ]]
 then
   echo "$response is to righ, try again"
-elif [[ $guess -lt 3 ]]
+elif [[ $guess -lt 0 ]]
 then
   echo "$guess is too low, try again"
 else
